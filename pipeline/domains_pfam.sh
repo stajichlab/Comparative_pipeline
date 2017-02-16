95e61140120a1954ae0876cc13e4d8ed29c890ff
Jason Stajich <jasonstajich.phd@gmail.com>
2017-02-15 22:44:44 -0800

    Run Pfam

---
+++ pipeline/domains_pfam.sh
@@ -0,0 +1,63 @@
+#!/usr/bin/bash
+
+#SBATCH --nodes 1 --ntasks 4
+#SBATCH --job-name=domains.Pfam
+#SBATCH --output=domains.Pfam.%A_%a.log
+
+
+if [ -f config.txt ]; then
+ source config.txt
+else
+ echo "need config file to set some project-specific variables"
+ exit
+fi
+
+if [ $DOMAINS ]; then
+ OUTDIR=$DOMAINS
+else
+ OUTDIR=domains
+fi
+
+mkdir -p $OUTDIR
+
+if [ ! $EXT ]; then
+ EXT=aa.fasta
+fi
+
+if [ ! $PROTEINS ]; then
+ PROTEINS=pep
+fi
+
+module load db-pfam
+module load hmmer/3
+
+if [ ! $PFAM_DB ]; then
+ echo "Need a PFAM_DB env variable either from config.txt or 'module load db-pfam'"
+ exit
+fi
+CPUS=$SLURM_CPUS_ON_NODE
+if [ ! $CPUS ]; then
+ CPUS=1
+fi
+
+IN=${SLURM_ARRAY_TASK_ID}
+
+if [ ! $IN ]; then
+ IN=$1
+ if [ ! $IN ]; then
+   IN=1
+   echo "defaulting to IN value is 1 - specify with --array or cmdline"
+ fi
+fi
+
+TOTAL=$(ls $PROTEINS/*.${EXT} | wc -l)
+if [ $IN -gt $TOTAL ]; then
+ echo "Only $TOTAL files in folder $PROTEINS, skipping $IN"
+ exit
+fi
+INFILE=$(ls $PROTEINS/*.${EXT} | sed -n ${IN}p)
+OUT=$DOMAINS/$(basename ${INFILE} .${EXT})
+
+if [ ! -f ${OUT}.hmmscan ]; then
+ hmmscan --cut_ga --cpu $CPUS --domtbl ${OUT}.domtbl -o ${OUT}.hmmscan $PFAM_DB/Pfam-A.hmm $INFILE
+fi
