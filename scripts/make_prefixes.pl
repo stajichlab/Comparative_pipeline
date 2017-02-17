383649e9bf9e915208537f0f1d06594365ef9e83
Jason Stajich <jasonstajich.phd@gmail.com>
2017-02-16 16:28:03 -0800

    update and doc prefix generation script

--- scripts/make_prefixes.pl
+++ scripts/make_prefixes.pl
@@ -1,12 +1,19 @@
 #!env perl
 use strict;
-my $dir= shift || "final_combine/pep";
+
+# This script will generate a table of prefix to long name for datasets
+# by parsing first sequence in fasta file and looking at header
+# looking for ">prefix|genename" and the table will have the form
+# prefix	FILENAME
+# attempts are make to remove version nummber encoding in the filename strings
+my $dir= shift || "pep";
 opendir(DIR, $dir) || die "dir is $dir: $!";
 print join ("\t", qw(Pref Name)),"\n";
 for my $file ( sort readdir(DIR) ) {
     next unless $file =~ /(\S+)\.(pep|aa|TFASTX)\.fasta$/;
     my $stem = $1;
     $stem =~ s/\.\w+\.v\d+//;
+    $stem =~ s/\.v\d+//;
 
     open(my $in => "head -n 1 $dir/$file |") || die $!;
     if( my $line = <$in> ) {
