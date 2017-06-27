76eb97dfc616e5b9fb25c50083cdf4b70e325b3d
Jason Stajich <jasonstajich.phd@gmail.com>
2017-06-26 23:25:51 -0700

    batch together more jobs - more efficient running

--- pipeline/run_BLASTP_pairs.sh
+++ pipeline/run_BLASTP_pairs.sh
@@ -5,7 +5,7 @@
 # THIS SCRIPT IS FOR RUNNING BLASTP for all pairs of split files
 
 module load ncbi-blast/2.6.0+
-module load orthomcl
+#module load orthomcl
 CPU=1
 if [ $SLURM_CPUS_ON_NODE ]; then
  CPU=$SLURM_CPUS_ON_NODE
@@ -17,24 +17,35 @@ BLAST=blastp
 JOBS=jobs.cmds
 OUTDIR=pair_compare
 INFASTA=in
+JOBSIZE=10
 CPUS=$SLURM_CPUS_ON_NODE
-
-N=${SLURM_ARRAY_TASK_ID}
-if [ ! $N ]; then
+N=0
+if [ ${SLURM_ARRAY_TASK_ID} ]; then
+ N=${SLURM_ARRAY_TASK_ID}
+elif [ $1 ]; then
  N=$1
 fi
 
-if [ ! $N ]; then
- N=1
+MAX=$(wc -l $JOBS | awk '{print $1}')
+B=$(expr $N \* $JOBSIZE)
+E=$(expr $B + $JOBSIZE - 1)
+if [ "$E" -gt "$MAX" ]; then
+ E=$MAX
+fi
+if [[ $B == 0 ]]; then 
+ B=1
 fi
 
-sed -n ${N}p $JOBS | while read DEST QUERY TARGET
+for R in $(seq $B 1 $E);
 do
- echo "$DEST.m9"
- if [ ! -f $DEST.m9 -a ! -s $DEST.m9 ]; then
-  time $BLAST -db $TARGET -query $QUERY -dbsize $DBSIZE -out $DEST.m9 -outfmt 6 -num_threads $CPU -evalue $E
- fi
- if [ ! -f $DEST.bpo ]; then
-  orthomclBlastParser $DEST.m9 $INFASTA > $DEST.bpo
- fi
+ sed -n ${R}p $JOBS | while read DEST QUERY TARGET
+ do
+  echo "$DEST.m9"
+  if [ ! -f $DEST.m9 -a ! -s $DEST.m9 ]; then
+   time $BLAST -db $TARGET -query $QUERY -dbsize $DBSIZE -out $DEST.m9 -outfmt 6 -num_threads $CPU -evalue $E
+  fi
+# if [ ! -f $DEST.bpo ]; then
+#  orthomclBlastParser $DEST.m9 $INFASTA > $DEST.bpo
+# fi
+ done
 done
