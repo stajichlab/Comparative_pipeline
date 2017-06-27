132e54445107fbfc287e3eca4aeb4c177283529e
Jason Stajich <jasonstajich.phd@gmail.com>
2017-06-26 23:17:44 -0700

    add CAZY processing and parsing output

---
+++ pipeline/domains_CAZY.sh
@@ -0,0 +1,64 @@
+#!/usr/bin/bash
+
+#SBATCH --nodes 1 --ntasks 2 --mem-per-cpu=1G
+#SBATCH --job-name=CAZY
+#SBATCH --output=domains.CAZY.%A_%a.log
+
+
+if [ -f config.txt ]; then
+ source config.txt
+else
+ echo "need config file to set some project-specific variables"
+ exit
+fi
+
+if [ $DOMAINS ]; then
+ OUTDIR=$DOMAINS
+else
+ OUTDIR=domains
+fi
+
+mkdir -p $OUTDIR/CAZY
+
+if [ ! $EXT ]; then
+ EXT=aa.fasta
+fi
+
+if [ ! $PROTEINS ]; then
+ PROTEINS=pep
+fi
+
+module load hmmer/3
+module load db-cazy
+
+if [ ! $CAZY_DB ]; then
+ echo "Need a CAZY_DB env variable either from config.txt or 'module load db-cazy'"
+ exit
+fi
+CPUS=$SLURM_CPUS_ON_NODE
+if [ ! $CPUS ]; then
+ CPUS=1
+fi
+
+IN=${SLURM_ARRAY_TASK_ID}
+
+if [ ! $IN ]; then
+ IN=$1
+ if [ ! $IN ]; then
+   IN=1
+   echo "defaulting to IN value is 1 - specify with --array or cmdline"
+ fi
+fi
+
+TOTAL=$(ls $PROTEINS/*.${EXT} | wc -l)
+if [ $IN -gt $TOTAL ]; then
+ echo "Only $TOTAL files in folder $PROTEINS, skipping $IN"
+ exit
+fi
+INFILE=$(ls $PROTEINS/*.${EXT} | sed -n ${IN}p)
+OUT=$OUTDIR/CAZY/$(basename ${INFILE} .${EXT})
+
+if [ ! -f ${OUT}.hmmscan ]; then
+ hmmscan --cpu $CPUS --domtbl ${OUT}.domtbl -o ${OUT}.hmmscan $CAZY_DB $INFILE
+ bash $CAZY_FOLDER/hmmscan-parser.sh ${OUT}.domtbl > ${OUT}.tsv
+fi
