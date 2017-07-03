e72fa9bff828cbfe98ca0b27d8da64b74c09095b
Jason Stajich <jasonstajich.phd@gmail.com>
2017-07-03 00:33:26 -0700

    more antismash directions

--- pipeline/run_antismash.sh
+++ pipeline/run_antismash.sh
@@ -1,10 +1,24 @@
 #!/usr/bin/bash
 
+#SBATCH --nodes 1 --ntasks 24 --mem 24G -p intel --time 3-0:00:00
+#SBATCH --job-name=AntiSMASH
+#SBATCH --output=AntiSMASH.%A_%a.log
 
-#SBATCH --nodes 1 --ntasks 2 --mem-per-cpu=1G
-#SBATCH --job-name=domains.Pfam
-#SBATCH --output=domains.Pfam.%A_%a.log
+CPUS=2
 
+if [ $SLURM_CPUS_ON_NODE ]; then
+ CPUS=$SLURM_CPUS_ON_NODE
+fi
+
+GENBANK=gbk
+EXT=gbk
+
+N=1
+if [ $1 ]; then
+ N=$1
+elif [ ${SLURM_ARRAY_TASK_ID} ]; then
+ N=${SLURM_ARRAY_TASK_ID}
+fi
 
 if [ -f config.txt ]; then
  source config.txt
@@ -13,22 +27,25 @@ else
  exit
 fi
 
-if [ $DOMAINS ]; then
- OUTDIR=$DOMAINS
-else
- OUTDIR=domains
+OUTDIR=secondary_metabolite
+if [ ! -d $OUTDIR ]; then
+ mkdir -p $OUTDIR
 fi
-
-mkdir -p $OUTDIR/Pfam
-
-if [ ! $EXT ]; then
- EXT=aa.fasta
+TOTAL=$(ls $GENBANK/*.${EXT} | wc -l)
+if [ $N -gt $TOTAL ]; then
+ echo "Only $TOTAL files in folder $GENBANK, skipping $N"
+ exit
+elif [[ $N == 0 ]]; then
+ echo "N must be between 1 and $TOTAL"
+ exit
 fi
-
-if [ ! $PROTEINS ]; then
- PROTEINS=pep
-fi
+INFILE=$(ls $GENBANK/*.${EXT} | sed -n ${N}p)
+echo "INFILE=$INFILE"
+OUT=$OUTDIR/$(basename ${INFILE} .${EXT})
+module unload openmpi
 module unload python
 module load antismash/4.0-branch
-
+export PYTHONHOME=$VIRTUAL_ENV
+echo "$VIRTUAL_ENV"
+srun -J AntiSmash --out AntiSmash.%A_$N.out --ntasks 24 --nodes 1 --mem 24G run_antismash.py -c $CPUS --taxon fungi --input-type nucl --clusterblast  --subclusterblast --smcogs --knownclusterblast --cassis --borderpredict --asf --outputfolder $OUT $INFILE
 
