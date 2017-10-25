9d4a3dd5470b91312220645ef3dc0b213cce6916
Jason Stajich <jasonstajich.phd@gmail.com>
2017-10-25 12:12:47 -0700

    run orthology summary in folder

--- pipeline/ortholog_summary.sh
+++ pipeline/ortholog_summary.sh
@@ -9,9 +9,10 @@ else
  echo "need config file to set some project-specific variables"
  exit
 fi
-
+ORTHOFOLDER=orthologs
+mkdir -p $ORTHOFOLDER
 if [ ! -f goodProteins.fasta ]; then
  module load hmmer/2
  cat $ORTHOINPUT/*.fasta | sreformat fasta - > goodProteins.fasta
 fi 
-perl $COMPAREFOLDER/scripts/orthomcl_to_table.pl -r $PROJECT.I${INFLATION}.ortholog_counts.tab -i $PROJECT.I${INFLATION}.mcl.out -db goodProteins.fasta --pfam ../domains/Pfam
+perl $COMPAREFOLDER/scripts/orthomcl_to_table.pl -r $PROJECT.I${INFLATION}.ortholog_counts.tab -i $ORTHOFOLDER/$PROJECT.I${INFLATION}.mcl.out -db goodProteins.fasta --pfam domains/Pfam
