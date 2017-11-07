9859f28feae2af6c3bb7fbb2b5e9ce3573f42976
YanWangTF <yanxw.wang@gmail.com>
2017-11-07 11:11:53 -0800

    added a largemem version of the orthagogue pipeline 'orthagogue_Largemem.sh' for sorting out large amount of comparison results

---
+++ pipeline/orthagogue_Largemem.sh
@@ -0,0 +1,28 @@
+#!/usr/bin/bash
+
+#SBATCH --time 2-0:00:00 --ntasks 8 --mem 350G
+module load orthagogue
+module load mcl
+
+if [ -f config.txt ]; then
+ source config.txt
+else
+ echo "need config file to set some project-specific variables"
+ exit
+fi
+
+CPU=8
+if  [ $SLURM_CPUS_ON_NODE ]; then
+ CPU=$SLURM_CPUS_ON_NODE
+fi
+
+if [ ! $PROJECT ]; then
+ echo "Need a project variable in config.txt"
+ exit
+fi
+
+if [ ! -f all.abc ]; then
+ orthAgogue -i $PROJECT.BLASTP -dbs 100000000 -s '|' -e 6 -c $CPU
+fi
+
+mcl all.abc -te $CPU --abc -I 1.5 -o $PROJECT.I15.mcl.out
