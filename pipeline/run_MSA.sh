f6e61a4cb5f3962176dbadd050608698b1a1016b
Jason Stajich <jasonstajich.phd@gmail.com>
2021-07-05 14:21:28 -0700

    use clipkit instead

--- pipeline/run_MSA.sh
+++ pipeline/run_MSA.sh
@@ -2,20 +2,20 @@
 #SBATCH --nodes 1 --ntasks 1 --mem 2G --time 2:00:00 -p short -J MSA
 
 module load muscle
-module load trimal
+module load clipkit
 MSATOOL="muscle -quiet"
 N=0
 if [ ${SLURM_ARRAY_TASK_ID} ]; then
- N=${SLURM_ARRAY_TASK_ID}
+    N=${SLURM_ARRAY_TASK_ID}
 elif [ $1 ]; then
- N=$1
+    N=$1
 fi
 EXT=fa
 ALNEXT=fasaln
 LIST=files.txt
 JOBSIZE=10
 if [ ! -f $LIST ]; then
- ls *.${EXT} > $LIST
+    ls *.${EXT} > $LIST
 fi
 
 MAX=$(wc -l $LIST | awk '{print $1}')
@@ -23,23 +23,25 @@ MAX=$(wc -l $LIST | awk '{print $1}')
 B=$(expr $N \* $JOBSIZE)
 E=$(expr $B + $JOBSIZE - 1)
 if [ "$E" -gt "$MAX" ]; then
- E=$MAX
+    E=$MAX
 fi
 if [[ $B == 0 ]]; then
- B=1
+    B=1
 fi
 
 for R in $(seq $B 1 $E);
 do
- sed -n ${R}p $LIST | while read FILENAME
- do
- STEM=$(basename $FILENAME .${EXT})
- DEST=$STEM.$ALNEXT
- TRIM=$STEM.trim
- if [ ! -f $DEST ]; then
-   $MSATOOL -in $FILENAME -out $DEST
-   trimal -in $DEST -out $TRIM -automated1
-   #perl -i -p -e 's/>(\S+)\|/>$1 /' $TRIM
- fi
- done
+    sed -n ${R}p $LIST | while read FILENAME
+    do
+	STEM=$(basename $FILENAME .${EXT})
+	DEST=$STEM.$ALNEXT
+	TRIM=$STEM.clipkit.trim
+	if [ ! -f $DEST ]; then
+	    $MSATOOL -in $FILENAME -out $DEST
+	    # clipkit instead?
+	    clipkit $DEST -o $TRIM
+	    #trimal -in $DEST -out $TRIM -automated1
+	    #perl -i -p -e 's/>(\S+)\|/>$1 /' $TRIM
+	fi
+    done
 done
