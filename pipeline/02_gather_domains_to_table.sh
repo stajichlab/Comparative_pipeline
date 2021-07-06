a59cd169383e1dd22eed44b246749899f1cef4e1
Jason Stajich <jasonstajich.phd@gmail.com>
2021-07-05 22:42:31 -0700

    parse domains out in a summary

---
+++ pipeline/02_gather_domains_to_table.sh
@@ -0,0 +1,7 @@
+#!/usr/bin/bash
+#SBATCH -p short -n 1 --mem 8gb --out logs/gather_MEROPS.log
+module load hmmer/3
+module load miniconda3
+perl scripts/gather_MEROPS_family_counts.pl --db pep --sfetch $(which esl-sfetch)
+perl scripts/gather_hmmscanMPI_domain_counts.pl
+perl scripts/gather_CAZY_counts.pl
---
+++ scripts/gather_hmmscanMPI_domain_counts.pl
@@ -0,0 +1,140 @@
+#!/usr/bin/env perl
+
+use strict;
+use warnings;
+use Getopt::Long;
+use List::Util qw(sum);
+
+use Getopt::Long;
+use Pod::Usage;
+
+my $man = 0;
+my $help = 0;
+my $sfetch = 'esl-sfetch';
+
+my ($indir,$out,$domaindir) = qw(domains/Pfam summary domain_seq/Pfam);
+my $outpref = 'Pfam';
+my $ext = '.domtbl';
+my $cutoff = 1e-2;
+my $seqdb;
+my $speciesorder;
+my $verbose;
+
+GetOptions('help|?'               => \$help, man => \$man,
+	   'i|input:s'            => \$indir,
+           'o|out:s'              => \$out,
+	   'op|outpref:s'         => \$outpref,
+	   'v|verbose!'           => \$verbose,
+	   'd|domain|domainout:s' => \$domaindir,
+	   'c|cutoff|evalue:s'    => \$cutoff,
+	   'ext|extension:s'      => \$ext,
+	   'db|database|seqs:s'   => \$seqdb,
+	   'species:s'            => \$speciesorder,
+	   'sfetch:s'             => \$sfetch,
+
+) or pod2usage(2);
+pod2usage(1) if $help;
+
+if ( $ext !~ /^\./) {
+  $ext = '.'. $ext;
+}
+
+mkdir($domaindir) unless -d $domaindir;
+
+my (%table, %table_genes,%specieset,%counts,%domain2acc);
+opendir(my $ind => $indir) || die "cannot open $indir: $!";
+for my $file ( readdir($ind) ) {
+    next unless $file =~ /(\S+)\Q$ext\E$/;
+    my $stem = $1;
+    $stem =~ s/(sp|var)\./$1\_/; # get rid of sp/var 
+    my ($species) = split(/\./,$stem);
+    warn("species=$species\n") if $verbose;
+    my $filepath = File::Spec->catfile($indir,$file);
+    open(my $in => $filepath ) || die "cannot open $filepath: $!";
+    while(<$in>) {		# parse domtbl file
+	next if /^\#/;		# skip comment lines
+	my ($gene_name,$qacc,$qlen,$domain,$domainacc,$tlen,
+	    $fullevalue,$fullscore,$fullbias,$n,$ntotal,$cvalue,$ivalue,
+	    $score,$dombias,
+	    $hstart,$hend, $qstart,$qend,$envfrom,$envto,$acc,$desc) =
+		split(/\s+/,$_,23);
+	$domain2acc{$domain} = $domainacc;
+	my $evalue = $ivalue;
+	if( $evalue > $cutoff ) {
+	    # skip
+	    next
+	}
+	$counts{$domain}->{$species}++;
+	push @{$table{$domain}->{$species}}, [$gene_name,$hstart,$hend];
+	$table_genes{$domain}->{$species}->{$gene_name}++;
+	$specieset{$species}++;
+    }
+}
+
+my @taxanames = sort keys %specieset;
+
+if ( $seqdb ) {
+    my $sfetchexe = `which $sfetch`;
+    chomp($sfetchexe);
+    if ( ! -x $sfetchexe ) {
+	warn("cannot find esl-sfetch executable");
+	$seqdb = undef;
+    }
+    elsif( ! -f "$seqdb/allseq.ssi" ) {
+	`cat $seqdb/*.fasta > $seqdb/allseq`;
+	`$sfetch --index $seqdb/allseq`;
+    }
+}
+
+mkdir($out) unless -d $out;
+open(my $fh => ">$out/$outpref\_counts.tsv") || die $!;
+open(my $fhgn => ">$out/$outpref\_counts_genes.tsv") || die $!;
+
+print $fh join("\t", qw(DOMAIN ACCESSION), @taxanames), "\n";
+print $fhgn join("\t", qw(DOMAIN ACCESSION), @taxanames), "\n";
+
+for my $s ( map { $_->[0] }
+	    sort { $b->[1] <=> $a->[1] }
+	    map { [$_, sum(values %{$counts{$_}})] } keys %table ) {
+
+    print $fh join("\t",$s,$domain2acc{$s},
+		   map { scalar @{$table{$s}->{$_} || []} } @taxanames), "\n";
+    print $fhgn join("\t",$s,$domain2acc{$s},
+		     map { scalar keys %{$table_genes{$s}->{$_} || {}} } 
+		     @taxanames), "\n";
+
+    if( $seqdb && -f "$seqdb/allseq.ssi" ) {
+	open(my $outseq => "| $sfetch -f $seqdb/allseq - > $domaindir/$s.fas") || die $!;
+	print $outseq join("\n", map { keys %{$table_genes{$s}->{$_} || {}} }
+			   @taxanames ), "\n";
+	close($outseq);
+    }
+}
+
+
+=head1 NAME
+
+gather_hmmscan_domain_counts.pl - generate a table of domain counts from hmmscan of Pfam or other DB
+
+=head1 USAGE
+
+perl gather_hmmscan_domain_counts.pl -i domains?Pfam -o table [-ext .domtbl]
+
+=head1 DESCRIPTION
+
+Generate table of domain counts for use in comparing, plotting as heatmap, etc
+
+=head2 ARGUMENTS
+
+-i / --input    Input folder  [One file per taxon/proteome]
+-o / --output   Output table
+-db /--database folder or file of sequences
+-ext            Extension of the result files. Default is .domtbl
+-c / --cutoff   evalue cutoff
+
+=head1 AUTHOR
+
+Jason Stajich - jasonstajich.phd[at]gmail.com
+University of California-Riverside
+
+=cut
